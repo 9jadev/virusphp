5b026ed63cd67be9fe0a04b843a6ac89c15b4f64
solomon <ahambasolomon800@gmail.com>
2021-08-31 10:30:39 +0100

    sndk

--- database/migrations/2021_08_30_161655_create_periods_table.php
+++ database/migrations/2021_08_30_161655_create_periods_table.php
@@ -16,6 +16,17 @@ class CreatePeriodsTable extends Migration
         Schema::create('periods', function (Blueprint $table) {
             $table->id();
             $table->string('day')->nullable();
+            $table->string("level")->nullable();
+            $table->string("7-8")->nullable();
+            $table->string("8-9")->nullable();
+            $table->string("9-10")->nullable();
+            $table->string("10-11")->nullable();
+            $table->string("11-12")->nullable();
+            $table->string("12-01")->nullable();
+            $table->string("01-02")->nullable();
+            $table->string("02-03")->nullable();
+            $table->string("03-04")->nullable();
+            $table->string("04-05")->nullable();
             $table->timestamps();
         });
     }
